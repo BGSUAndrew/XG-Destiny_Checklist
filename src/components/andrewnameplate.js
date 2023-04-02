3d77ab5dd4fa0a659eca098c499ac182efeb1b7e
Andrew O'Connor <andrew.oconnor.12@gmail.com>
2023-04-02 09:47:06 -0700

    1.7.3 Guardian Ranks Update
    Added Guardian Ranks

--- src/components/andrewnameplate.js
+++ src/components/andrewnameplate.js
@@ -4,13 +4,10 @@ class AndrewName extends Component {
     
         state = {
             loading: true,
-            nameplate: [],
-            dateLastPlayed: [],
-            gamertag: []
         }
 
       async componentDidMount() {
-        const url = "https://www.bungie.net/Platform/Destiny2/1/Profile/4611686018430407175/?components=100,200";
+        const url = "https://www.bungie.net/Platform/Destiny2/1/Profile/4611686018430407175/?components=100,200,1400";
         const response = await fetch(url, {
             headers: {
                 'x-api-key': '2c6c008e57644a4bb63f00504758c443'
@@ -19,8 +16,14 @@ class AndrewName extends Component {
         const data = await response.json();
         this.setState({nameplate: data.Response.characters.data["2305843009263048442"].emblemBackgroundPath});
         this.setState({dateLastPlayed:data.Response.profile.data.dateLastPlayed});
-        this.setState({gamertag:data.Response.profile.data.userInfo.displayName})
-        console.log(data.Response.profile.data.userInfo.displayName) 
+        this.setState({gamertag:data.Response.profile.data.userInfo.displayName});
+        this.setState({guardianRank:data.Response.profile.data.currentGuardianRank});
+        this.setState({ally:data.Response.profileCommendations.data.commendationNodeScoresByHash["154475713"]});
+        this.setState({fun:data.Response.profileCommendations.data.commendationNodeScoresByHash["1341823550"]});
+        this.setState({mastery:data.Response.profileCommendations.data.commendationNodeScoresByHash["4180748446"]});
+        this.setState({leadership:data.Response.profileCommendations.data.commendationNodeScoresByHash["1390663518"]});
+
+        console.log(data.Response.profileCommendations.data.commendationNodeScoresByHash["154475713"]) 
        
       }
     
@@ -29,12 +32,21 @@ class AndrewName extends Component {
         const bungie = "https://bungie.net"
         const dateLastPlayed = this.state.dateLastPlayed
         const gamertag = this.state.gamertag
+        const guardianRank = this.state.guardianRank
+        const ally = this.state.ally
+        const fun = this.state.fun
+        const mastery = this.state.mastery
+        const leadership = this.state.leadership
         return (
+          <section>
             <div>
                 <h1 id='andrew' className='text-center'>{gamertag}</h1>
                <img className='img-fluid' src={bungie + nameplate} alt='Image' />
-               <p>Last logged in: {dateLastPlayed}</p>
+               <p className='text-center'>Last logged in: {dateLastPlayed}</p>
+               <p className='text-center'>Guardian Rank: {guardianRank}</p>
             </div>
+
+          </section>
           )
       }
     }
--- src/components/joenameplate.js
+++ src/components/joenameplate.js
@@ -4,9 +4,6 @@ class JoeName extends Component {
     
         state = {
             loading: true,
-            nameplate: [],
-            dateLastPlayed: [],
-            gamertag: []
         }
 
       async componentDidMount() {
@@ -19,8 +16,13 @@ class JoeName extends Component {
         const data = await response.json();
         this.setState({nameplate: data.Response.characters.data["2305843009889894407"].emblemBackgroundPath});
         this.setState({dateLastPlayed:data.Response.profile.data.dateLastPlayed});
-        this.setState({gamertag:data.Response.profile.data.userInfo.displayName})
-        console.log(data.Response.profile.data.userInfo.displayName) 
+        this.setState({gamertag:data.Response.profile.data.userInfo.displayName});
+        this.setState({guardianRank:data.Response.profile.data.currentGuardianRank}) 
+        this.setState({ally:data.Response.profileCommendations.data.commendationNodeScoresByHash["154475713"]});
+        this.setState({fun:data.Response.profileCommendations.data.commendationNodeScoresByHash["1341823550"]});
+        this.setState({mastery:data.Response.profileCommendations.data.commendationNodeScoresByHash["4180748446"]});
+        this.setState({leadership:data.Response.profileCommendations.data.commendationNodeScoresByHash["1390663518"]});
+        //console.log(data.Response.profile.data.userInfo.displayName) 
        
       }
     
@@ -29,11 +31,17 @@ class JoeName extends Component {
         const bungie = "https://bungie.net"
         const dateLastPlayed = this.state.dateLastPlayed
         const gamertag = this.state.gamertag
+        const guardianRank = this.state.guardianRank
+        const ally = this.state.ally
+        const fun = this.state.fun
+        const mastery = this.state.mastery
+        const leadership = this.state.leadership
         return (
             <div>
                 <h1 id='joe' className='text-center'>{gamertag}</h1>
                <img className='img-fluid' src={bungie + nameplate} alt='Image' />
-               <p>Last logged in: {dateLastPlayed}</p>
+               <p className='text-center'>Last logged in: {dateLastPlayed}</p>
+               <p className='text-center'>Guardian Rank: {guardianRank}</p>
             </div>
           )
       }
