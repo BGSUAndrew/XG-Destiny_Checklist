64c5dcb0f1bdd7ae9b648afd506a905354bad34a
Joe Mechenbier <joemech22@gmail.com>
2024-03-01 09:21:31 -0500

    idk

---
+++ src/routes/weaponchasing.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Sidebar from '../components/Sidebar';
+
+
+class ActivityReport extends Component {
+    render() {
+        return (
+            
+            <div id="outer-container">
+      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+            <h1 className='page_title'>Weapon Chasing</h1>
+
+            <div className='data_container'>
+            </div>
+
+
+            </div>
+
+        );
+    }
+}
+
+export default weaponchasing;
