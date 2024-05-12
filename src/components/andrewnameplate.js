0fa279f43678bc6aca238bd7fb4c5240f82cec72
Andrew O'Connor <andrew.oconnor.12@gmail.com>
2024-05-12 11:41:36 -0700

    Updating my name plate to React Hooks
    Rewriting most of this app in React Hooks. This is the first step.

--- src/components/andrewnameplate.js
+++ src/components/andrewnameplate.js
@@ -1,49 +1,47 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 
-class AndrewName extends Component {
-    
-        state = {
-            loading: true,
+function AndrewName() {
+      const bungie = "https://bungie.net"
+      const [nameplate, setNameplate] = useState(null);
+      const [gamertag, setGamertag] = useState(null);
+      const [guardianRank, setGuardianRank] = useState("1");
+      const [lastPlayedDate, setDateLastPlayed] = useState(null);
+      const [ally, setAlly] = useState(0);
+      const [fun, setFun] = useState(0);
+      const [mastery, setMastery] = useState(0);
+      const [leadership, setLeadership] = useState(0);
+      const newDate = new Date(lastPlayedDate)
+      const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+      const lastPlayedDateUpdate = newDate.toLocaleDateString(undefined, options);
+      
+       useEffect(() => {
+        const fetchData = async() => {
+          const url = "https://www.bungie.net/Platform/Destiny2/1/Profile/4611686018430407175/?components=100,200,1400";
+          const getData = await fetch(url, {
+              headers: {
+                  'x-api-key': '2c6c008e57644a4bb63f00504758c443'
+              }
+          })
+          getData.json().then(json => {
+            setNameplate(json.Response.characters.data["2305843009263048442"].emblemBackgroundPath);
+            setGamertag(json.Response.profile.data.userInfo.bungieGlobalDisplayName);
+            setGuardianRank(json.Response.profile.data.currentGuardianRank);
+            setDateLastPlayed(json.Response.profile.data.dateLastPlayed);
+            setAlly(json.Response.profileCommendations.data.commendationNodeScoresByHash["154475713"]);
+            setFun(json.Response.profileCommendations.data.commendationNodeScoresByHash["1341823550"]);
+            setMastery(json.Response.profileCommendations.data.commendationNodeScoresByHash["4180748446"]);
+            setLeadership(json.Response.profileCommendations.data.commendationNodeScoresByHash["1390663518"]);
+          });
         }
-
-      async componentDidMount() {
-        const url = "https://www.bungie.net/Platform/Destiny2/1/Profile/4611686018430407175/?components=100,200,1400";
-        const response = await fetch(url, {
-            headers: {
-                'x-api-key': '2c6c008e57644a4bb63f00504758c443'
-            }
-        })
-        const data = await response.json();
-        this.setState({nameplate: data.Response.characters.data["2305843009263048442"].emblemBackgroundPath});
-        this.setState({gamertag:data.Response.profile.data.userInfo.bungieGlobalDisplayName});
-        this.setState({guardianRank:data.Response.profile.data.currentGuardianRank});
-        this.setState({dateLastPlayed: data.Response.profile.data.dateLastPlayed});
-        this.setState({ally:data.Response.profileCommendations.data.commendationNodeScoresByHash["154475713"]});
-        this.setState({fun:data.Response.profileCommendations.data.commendationNodeScoresByHash["1341823550"]});
-        this.setState({mastery:data.Response.profileCommendations.data.commendationNodeScoresByHash["4180748446"]});
-        this.setState({leadership:data.Response.profileCommendations.data.commendationNodeScoresByHash["1390663518"]});
-      }
-
-      render() {
-        const nameplate = this.state.nameplate
-        const bungie = "https://bungie.net"
-        const gamertag = this.state.gamertag
-        const guardianRank = this.state.guardianRank
-        const ally = this.state.ally
-        const fun = this.state.fun
-        const mastery = this.state.mastery
-        const leadership = this.state.leadership
-        const lastPlayed = this.state.dateLastPlayed;
-        const newDate = new Date(lastPlayed);
-        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        const lastPlayedDate = newDate.toLocaleDateString(undefined, options);
+        fetchData();
+       },[]);
 
         return (
           <section>
             <div>
                 <h1 id='andrew' className='text-center'>{gamertag}</h1>
                <img className='img-fluid' src={bungie + nameplate} alt='Image' />
-               <p className='text-center'>Last logged in: {lastPlayedDate}</p>
+               <p className='text-center'>Last logged in: {lastPlayedDateUpdate}</p>
                <p className='text-center'>Guardian Rank: <span className='font-bold'>{guardianRank}</span></p>
             </div>
             <div className='data_container tenpx_gap'>
@@ -56,6 +54,6 @@ class AndrewName extends Component {
           </section>
           )
       }
-    }
+    
 
 export default AndrewName;
