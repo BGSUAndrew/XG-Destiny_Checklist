f815b1204caeea0f7927eb55dc424be0f064e861
Andrew O'Connor <andrew.oconnor.12@gmail.com>
2025-01-02 15:54:18 -0800

    Updated packages
    Updated packages and pulled down began set up on dungeon stats page.

---
+++ src/routes/dungeonstats.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import Sidebar from '../components/Sidebar';
+import Seasonal from '../components/seasonal'
+
+function DungeonStats() {
+    const alertName = () => {
+        alert('Hello world');
+    };
+return (
+    <div id="outer-container">
+      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      <Seasonal/>
+    <div>
+        <h1>Coming Soon!</h1>
+        <button onClick={alertName}>
+            Click to load alert
+        </button>
+    </div>
+    </div>
+)
+}
+
+export default DungeonStats;
+
+
