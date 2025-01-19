f879e6e5339f50083e7f0284a32fac61e039735d
Andrew O'Connor <andrew.oconnor.12@gmail.com>
2025-01-19 13:16:20 -0800

    Updating Dungeon page to include Joe and Ian
    Description

--- src/routes/dungeonstats.js
+++ src/routes/dungeonstats.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Sidebar from '../components/Sidebar';
 import Seasonal from '../components/seasonal'
 import AndrewDungeonStats from '../components/andrewdungeondata';
+import IanDungeonStats from '../components/iandungoendata';
+import JoeDungeonStats from '../components/joedungeonstats';
 
 function DungeonStats() {
     const alertName = () => {
@@ -11,13 +13,12 @@ return (
     <div id="outer-container">
       <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
       <Seasonal/>
-    <div>
-        <h1>Coming Soon!</h1>
-        <button onClick={alertName}>
-            Click to load alert!
-        </button>
-    </div>
+    <div className='data_container'>
     <AndrewDungeonStats></AndrewDungeonStats>
+    <IanDungeonStats></IanDungeonStats>
+    <JoeDungeonStats></JoeDungeonStats>
+    </div>
+
     </div>
 )
 }
